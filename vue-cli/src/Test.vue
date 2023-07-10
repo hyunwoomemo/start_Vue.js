962e04e8d2d0387748be99bd03d759a606e7928e
hyunwoomemo <archihw94@gmail.com>
2023-07-10 09:07:50 +0900

    vue-cli 학습

---
+++ vue-cli/src/Test.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>header</div>
+
+<script>
+export default {
+methods: {
+  addNum: function(){}
+}
+}
+</script>
+
+<style>
+/* css */
+</style>
---
+++ vue-cli/src/a.vue
@@ -0,0 +1,14 @@
+<template>
+  <!-- html -->
+  <div>header</div>
+</template>
+
+<script>
+export default {
+// javascript
+}
+</script>
+
+<style>
+/* css */
+</style>
