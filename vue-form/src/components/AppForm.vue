f115c9a23aa079625e6841e81b47d996ec931aaa
hyunwoomemo <archihw94@gmail.com>
2023-07-10 12:53:17 +0900

    vue-form 및 learn-vue-js 강좌 마무리

---
+++ vue-form/src/components/AppForm.vue
@@ -0,0 +1,43 @@
+<template>
+  <form action="" v-on:submit.prevent="">
+    <div>
+      <label for="id">id</label>
+      <input type="text" v-model="idValue" id="id">
+      {{ idValue }}
+    </div>
+    <div>
+      <label for="pw">passwprd</label>
+      <input type="password" v-model="pwValue" id="pw">
+      {{ pwValue }}
+    </div>
+    <button>submit</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      idValue: '',
+      pwValue: '',
+    }
+  },
+  methods: {
+    handleSubmit: function (e) {
+      e.preventDefault();
+    }
+  }
+}
+</script>
+
+<style scoped>
+form {
+  display: flex;
+  gap: 1rem;
+
+  > div {
+    display: flex;
+    gap: 1rem;
+  }
+}
+</style>
